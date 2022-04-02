b79e904796ce154abc8e99c4018603f3f950eee1
Leca Valentin-Silviu <valentin.leca@stud.acs.upb.ro>
2022-04-02 17:12:08 +0300

    implemented functionality for customized message and fade effect

--- labtasks/MyFirstAndroidApplication/app/src/main/java/ro/pub/cs/systems/eim/lab01/myfirstandroidapplication/MainActivity.java
+++ labtasks/MyFirstAndroidApplication/app/src/main/java/ro/pub/cs/systems/eim/lab01/myfirstandroidapplication/MainActivity.java
@@ -3,15 +3,19 @@ package ro.pub.cs.systems.eim.lab01.myfirstandroidapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    final private static long TRANSPARENCY_EFFECT_DURATION = 5000;
+    final private static long TRANSPARENCY_EFFECT_DURATION = 500;
 
-    private ButtonClickListener buttonClickListener = new ButtonClickListener();
+    final private ButtonClickListener buttonClickListener = new ButtonClickListener();
+
+    private static int count = 0;
+    private static String a;
 
     private class ButtonClickListener implements Button.OnClickListener {
 
@@ -23,7 +27,21 @@ public class MainActivity extends AppCompatActivity {
 
             // TODO: exercise 9a
 
+
+            if (count == 0) {
+                greetingTextView.setText(greetingTextView.getText().toString().replace("xxx", "\n" + userNameEditText.getText()));
+                count++;
+            }
+            else
+                greetingTextView.setText(greetingTextView.getText().toString().replace(a, "\n"+userNameEditText.getText()));
+
+            a = "\n"+userNameEditText.getText();
             // TODO: exercise 9b
+
+            AlphaAnimation fadeEffect = new AlphaAnimation(1.0f, 0.0f);
+            fadeEffect.setDuration(TRANSPARENCY_EFFECT_DURATION);
+            fadeEffect.setFillAfter(true);
+            greetingTextView.setAnimation(fadeEffect);
         }
 
     }
